54a80fee6999fa18928743d9d0d9fccf88cbaed0
Mrityunjay243 <mrityunjayjoshi565@gmail.com>
2024-08-05 23:02:51 -0700

    Some more routes added to users

--- PayTM-App/backend/routes/user.js
+++ PayTM-App/backend/routes/user.js
@@ -84,4 +84,51 @@ router.post("/signin", async (req, res) => {
         message: "Error while logging in"
     })
 })
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put('/', async (req, res) => {
+    const { success } = updateBody.safeParse(req.body);
+    if (!success){
+        res.status(411).json({
+            message: "error while updating information"
+        })
+    }
+    await User.updateOne({ _id: req.userId }, req.body);
+
+    res.json({
+        message: "Updated Successfully"
+    })
+});
+
+// router too get users from the database filterable based on first name and last name
+router.get('/bulk', async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username, 
+            firstName: user.firstName, 
+            lastName: user.lastName,
+            _id: user._id   
+        }))
+    });
+})
+
 module.exports = user;
